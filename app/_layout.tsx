19e992c55d3d38663801603ce06f3e3a184dc8e1
amirhossein <79211332+capamir@users.noreply.github.com>
2025-07-09 12:42:28 +0330

    layout and navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,24 @@ import { Stack } from "expo-router";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      {/* <StatusBar hidden={true} /> */}
+
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="movie/[id]"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
+  );
 }

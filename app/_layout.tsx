a3f342eb54ec0785164a3b03594c734a4b2cde5f
amirhossein <79211332+capamir@users.noreply.github.com>
2025-07-10 14:40:17 +0330

    hidden status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import "./global.css";
 
 export default function RootLayout() {
   return (
     <>
-      {/* <StatusBar hidden={true} /> */}
+      <StatusBar hidden={true} />
 
       <Stack>
         <Stack.Screen
